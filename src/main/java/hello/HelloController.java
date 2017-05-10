e4ba0db852d39d207cd15799b957a39a6d086454
Joel Steigerwald <Joel.Steigerwald@bestbuy.com>
2017-05-10 11:02:51 -0500

    Change to use traditional war file deployment

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,18 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+    private static Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/")
     public String index() {
+        log.info("Made It Here");
         return "Greetings from Spring Boot!";
     }
 
